34608a0c279be23f41b76ac6f097ee18fdc472bf
Jen Bourey <jennifer.bourey@gmail.com>
2011-05-06 19:21:55 +0000

    UMOBILE-59 Adding initial DeviceProxy to control network error handling

---
+++ Resources/js/models/DeviceProxy.js
@@ -0,0 +1,28 @@
+
+
+var DeviceProxy = function (facade) {
+    var app = facade, self = {};
+    
+    init = function () {
+        // nothing to see here
+    };
+    
+    self.checkNetwork = function() {
+        if (!Ti.Network.online) {
+            alertDialog = Titanium.UI.createAlertDialog({
+                title: app.localDictionary.error,
+                message: app.localDictionary.networkConnectionRequired,
+                buttonNames: [app.localDictionary.OK]
+            });
+            alertDialog.show();
+            Ti.API.debug("Network is offline");
+            return false;
+        } else {
+            return true;
+        }
+    };
+    
+    init();
+    
+    return self;
+};
