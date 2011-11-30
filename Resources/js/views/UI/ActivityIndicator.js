55d3d9e6a9b5202d56a39b52c142ad0e122cdd4c
Jeff Cross <middlefloor@gmail.com>
2011-11-29 17:20:10 -0700

    Removed UI.js and added ActivityIndicator.js

---
+++ Resources/js/views/UI/ActivityIndicator.js
@@ -0,0 +1,36 @@
+var messageLabel,
+    indicator = {view: Ti.UI.createView(app.styles.globalActivityIndicator)},
+    dialog = Ti.UI.createView(app.styles.activityIndicatorDialog);
+
+exports.view = indicator.view;
+
+indicator.view.add(dialog);
+
+messageLabel = Ti.UI.createLabel(app.styles.activityIndicatorMessage);
+messageLabel.text = app.localDictionary.loading;
+dialog.add(messageLabel);
+
+exports.setLoadingMessage = function (m) {
+    if (typeof m == 'string') {
+        messageLabel.text = m;
+    }
+    else {
+        Ti.API.error("Message isn't valid:" + m + ' ' + typeof m);
+    }
+};
+
+exports.destroy = function () {
+    Ti.App.removeEventListener(app.events['DIMENSION_CHANGES'], resetActivityIndicator);
+};
+
+exports.resetDimensions = function () {
+    indicator.view.top = app.styles.globalActivityIndicator.top;
+    indicator.view.height = app.styles.globalActivityIndicator.height;
+    indicator.view.width = app.styles.globalActivityIndicator.width;
+    dialog.width = app.styles.activityIndicatorDialog.width;
+};
+
+function resetActivityIndicator (e) {
+    indicator.resetDimensions();
+}
+Titanium.App.addEventListener(app.events['DIMENSION_CHANGES'], resetActivityIndicator);
