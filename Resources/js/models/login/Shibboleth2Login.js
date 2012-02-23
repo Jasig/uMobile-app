e6d9b5717fe92238f40aa6d5aafb2faa4ccd7565
Jen Bourey <jennifer.bourey@gmail.com>
2012-02-23 08:25:11 -0800

    NOJIRA Updating license headers and setting version number to 1.1 RC1

--- Resources/js/models/login/Shibboleth2Login.js
+++ Resources/js/models/login/Shibboleth2Login.js
@@ -1,4 +1,22 @@
 /*
+ * Licensed to Jasig under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work
+ * for additional information regarding copyright ownership.
+ * Jasig licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a
+ * copy of the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+/*
 This method is only known to work with Titanium SDK 1.7.5, 
 and logout only works if the Ti.Network.HTTPClient.clearCookies method
 has been implemented. It will be tested with later versions of the Titanium SDK
